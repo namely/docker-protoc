851c61e88a4375aa591a336139a9f376d81a3182
Gian Biondi <gian@namely.com>
2016-02-10 11:52:21 -0500

    Fancier status messages

--- golang/script.sh
+++ golang/script.sh
@@ -1,13 +1,20 @@
 #!/bin/sh
 
+set -e
+
 if [ ! -f *.proto ]; then
   echo "No proto files found!"
   exit 1
 fi
 
 echo "Found Proto definitions:"
-ls *.proto
+for p in *.proto
+do
+  echo -e "\t+$p"
+done
 
-echo "Building..."
+echo 
+
+echo "Building Go source..."
 protoc -I /defs /defs/*.proto --go_out=plugins=grpc:.
-echo "Done"
+echo "Done!"
--- ruby/script.sh
+++ ruby/script.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
+set -e
+
 if [ ! -f *.proto ]; then
   echo "No proto files found!"
   exit 1
 fi
 
 echo "Found Proto definitions:"
-ls *.proto
+for p in *.proto
+do
+  echo -e "\t+$p"
+done
+
+echo 
 
 echo "Building..."
-#protoc -I /defs /defs/*.proto --ruby_out=./ --grpc_out=./ --plugin=protoc-gen-grpc=`which grpc_ruby_plugin`
 protoc -I /defs /defs/*.proto --ruby_out=./ --grpc_out=./ --plugin=protoc-gen-grpc=/opt/namely/grpc_ruby_plugin
 
 echo "Done"
