70681d9aad43128ed74af7d3d55dc41b48547191
Gian Biondi <gian@namely.com>
2016-03-08 16:15:19 -0500

    Adds python plugins

--- build.sh
+++ build.sh
@@ -4,17 +4,26 @@ set -e
 
 echo "Building Docker containers"
 
-REGISTRY='namely'
-BASE_IMAGE='protoc'
+declare -a DIRS=( 'golang' 'ruby' 'python' )
+declare -a IMAGES=( 'protoc-go' 'protoc-ruby' 'protoc-python' )
+
 GOLANG_IMAGE='protoc-go'
 RUBY_IMAGE='protoc-ruby'
+PYTHON_IMAGE='protoc-python'
+
+REGISTRY='namely'
+BASE_IMAGE='protoc'
 TAG=$(git rev-parse --short HEAD)
 TAG='latest'
 
 buildAll () {
   docker build -t $REGISTRY/$BASE_IMAGE:$TAG .
-  docker build -t $REGISTRY/$RUBY_IMAGE:$TAG ./ruby
-  docker build -t $REGISTRY/$GOLANG_IMAGE:$TAG ./golang
+  for i in ${!IMAGES[@]};
+  do
+    echo
+    echo "Building ${IMAGES[$i]}... "
+    docker build -t $REGISTRY/${IMAGES[$i]}:$TAG ./${DIRS[$i]}
+  done
 }
 
 #Read from args
