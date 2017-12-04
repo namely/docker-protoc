45851317cc0ab38cce04f4ce10984711316e6293
Martin Kess <martinkess@gmail.com>
2017-12-04 16:04:15 -0500

    Fixes to grpc-gateway generation (#30)
    * gRPC gateway generation now go specific, and generates in same directory as protos.
    
    * refactored test.sh
    
    * Test for generating dir

--- test.sh
+++ test.sh
@@ -2,19 +2,37 @@
 
 LANGS=("go" "ruby" "csharp" "java" "python" "objc")
 
+# Checks that directories were appropriately created, and deletes the generated directory.
+testGeneration() {
+  lang=$1
+  extra_arg=$2
+  echo "Testing language $lang $extra_arg"
+
+  # Test calling a file directly.
+  docker run --rm -v=`pwd`:/defs namely/test-protoc-all -f test/test.proto -l $lang -i test $extra_arg
+  if [[ ! -d "gen/pb-$lang" ]]; then
+      echo "generated directory does not exist"
+      exit 1
+  fi
+  rm -rf gen/
+
+  # Test scanning a dir.
+  docker run --rm -v=`pwd`:/defs namely/test-protoc-all -d test -l $lang -o gen/dir/$lang $extra_arg
+  if [[ ! -d gen/dir/$lang ]]; then
+    echo "generated directory for all-file include method does not exist"
+    exit 1
+  fi
+  rm -rf gen/
+
+  echo "Passed!"
+}
+
 docker build -t namely/test-protoc-all ./all
+
+# Test grpc-gateway generation.
+testGeneration go --with-gateway
 
 # Generate proto files
 for lang in ${LANGS[@]}; do
-    echo "Testing language $lang"
-    docker run --rm -v=`pwd`:/defs namely/test-protoc-all -f test/test.proto -l $lang -i test
-    docker run --rm -v=`pwd`:/defs namely/test-protoc-all -d test -l $lang -o gen/dir/$lang --with-gateway
-    if [[ ! -d gen/pb-$lang ]]; then
-        echo "generated directory does not exist"
-        exit 1
-    fi
-    if [[ ! -d gen/dir/$lang ]]; then
-        echo "generated directory for all-file include method does not exist"
-        exit 1
-    fi
+    testGeneration "$lang"
 done
