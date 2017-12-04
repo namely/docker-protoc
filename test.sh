1966f7331adc00ca31ade7b942b6190882d3d4e5
Martin Kess <martin.kess@namely.com>
2017-12-04 15:57:45 -0500

    Test for generating dir

--- test.sh
+++ test.sh
@@ -8,15 +8,23 @@ testGeneration() {
   extra_arg=$2
   echo "Testing language $lang $extra_arg"
 
+  # Test calling a file directly.
   docker run --rm -v=`pwd`:/defs namely/test-protoc-all -f test/test.proto -l $lang -i test $extra_arg
-
   if [[ ! -d "gen/pb-$lang" ]]; then
       echo "generated directory does not exist"
       exit 1
   fi
-  echo "Passed!"
-
   rm -rf gen/
+
+  # Test scanning a dir.
+  docker run --rm -v=`pwd`:/defs namely/test-protoc-all -d test -l $lang -o gen/dir/$lang $extra_arg
+  if [[ ! -d gen/dir/$lang ]]; then
+    echo "generated directory for all-file include method does not exist"
+    exit 1
+  fi
+  rm -rf gen/
+
+  echo "Passed!"
 }
 
 docker build -t namely/test-protoc-all ./all
