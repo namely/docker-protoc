72d964ffc1c9671ccf112c6e78267d87a1d4f620
Martin Kess <martin.kess@namely.com>
2018-01-25 15:55:09 -0500

    Added grpc_cli dockerfile

---
+++ cli/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+# This script wraps grpc_cli so we can do some setup before running it.
+
+# Since we have mounted /defs to a directory on the user's host machine, we can't
+# create symlinks there, or they will be created on the host machine. So symlink
+# everything into the /run directory, and run grpc_cli from there.
+
+ln -sf /defs/* /run/
+ln -sf /protos/* /run/
+
+grpc_cli $@
