8338d388e5503464ab217135267b37165595cba5
Martin Kess <martin.kess@namely.com>
2018-03-15 14:19:11 -0400

    Fixed python generation with output dir specified.

--- all/test.sh
+++ all/test.sh
@@ -6,43 +6,52 @@ CONTAINER=$1
 
 # Checks that directories were appropriately created, and deletes the generated directory.
 testGeneration() {
-  lang=$1
-  extra_arg=$2
-  echo "Testing language $lang $extra_arg"
+    lang=$1
+    shift
+    expected_output_dir=$1
+    shift
+    extra_args=$@
+    echo "Testing language $lang $expected_output_dir $extra_args"
 
-  # Test calling a file directly.
-  docker run --rm -v=`pwd`:/defs $CONTAINER -f test/test.proto -l $lang -i test $extra_arg
-  if [[ "$lang" == "python" ]]; then
-      if [[ ! -d "gen/pb_$lang" ]]; then
-          echo "generated directory does not exist"
-          exit 1
-       elif [[ ! -f "gen/__init__.py" ]] || [[ ! -f "gen/pb_$lang/__init__.py" ]]; then
-          echo "__init__.py files were not generated."
-          exit 1
-       fi
-  elif [[ ! -d "gen/pb-$lang" ]]; then 
-      echo "generated directory does not exist"
-      exit 1
-  fi
-  rm -rf gen/
+    # Test calling a file directly.
+    docker run --rm -v=`pwd`:/defs $CONTAINER -f test/test.proto -l $lang -i test $extra_args
+    if [[ ! -d "$expected_output_dir" ]]; then 
+        echo "generated directory $expected_output_dir does not exist"
+        exit 1
+    fi
 
-  # Test scanning a dir.
-  docker run --rm -v=`pwd`:/defs $CONTAINER -d test -l $lang -o gen/dir/$lang $extra_arg
-  if [[ ! -d gen/dir/$lang ]]; then
-    echo "generated directory for all-file include method does not exist"
-    exit 1
-  fi
-  rm -rf gen/
-
-  echo "Passed!"
+    if [[ "$lang" == "python" ]]; then
+        # Test that we have generated the __init__.py files.
+        current_path="$expected_output_dir"
+        while [[ $current_path != "." ]]; do
+          if [[ ! -f "$current_path/__init__.py" ]]; then
+              echo "__init__.py files were not generated in $current_path"
+              exit 1
+          fi
+          current_path=$(dirname $current_path)
+        done
+    fi
+    rm -rf `echo $expected_output_dir | cut -d '/' -f1`
+    echo "Generating for $lang passed!"
 }
 
-# Test grpc-gateway generation.
-testGeneration go --with-gateway
+# Test grpc-gateway generation (only valid for Go)
+testGeneration go "gen/pb-go" --with-gateway
 
 # Generate proto files
 for lang in ${LANGS[@]}; do
-    testGeneration "$lang"
-done
+    expected_output_dir=""
+    if [[ "$lang" == "python" ]]; then
+        expected_output_dir="gen/pb_$lang"
+    else
+      expected_output_dir="gen/pb-$lang"
+    fi
 
+    # Test without an output directory.
+    testGeneration "$lang" "$expected_output_dir"
+
+    # Test with an output directory.
+    test_dir="gen/foo/bar"
+    testGeneration "$lang" "$test_dir" -o "$test_dir"
+done
 
