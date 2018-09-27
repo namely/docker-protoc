a3811ac9fd639b9f8e5e506bb6f69994a897aac0
Mitsuhiro Koga <shiena.jp@gmail.com>
2018-09-27 14:45:30 +0900

    Clone from release tag instead of branch

--- all/install-protobuf.sh
+++ all/install-protobuf.sh
@@ -5,6 +5,11 @@ if [ -z $1 ]; then
     exit 1
 fi
 
+if [ -z $2 ]; then
+    echo "You must specify a grpc-java version."
+    exit 1
+fi
+
 curl -sSL https://github.com/gflags/gflags/archive/v2.2.1.tar.gz -o gflags.tar.gz && tar -xzvf gflags.tar.gz
 cd gflags-2.2.1/
 cmake .
@@ -13,7 +18,7 @@ make install
 
 cd /tmp
 
-git clone -b v$1.x --recursive -j8 https://github.com/grpc/grpc
+git clone -b $1 --recursive -j8 https://github.com/grpc/grpc
 cd /tmp/grpc
 make
 make install
@@ -33,6 +38,6 @@ cd /tmp/grpc
 make grpc_cli
 
 cd /tmp
-git clone -b v$1.x --recursive https://github.com/grpc/grpc-java.git
+git clone -b $2 --recursive https://github.com/grpc/grpc-java.git
 cd /tmp/grpc-java/compiler
 ../gradlew java_pluginExecutable
