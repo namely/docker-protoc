50077cbdcf7d89d3a3bbd6868ee1ffbe1a9c2590
Martin Kess <martin.kess@namely.com>
2019-04-25 09:25:00 -0400

    Allow output of descriptor proto

--- all/entrypoint.sh
+++ all/entrypoint.sh
@@ -24,7 +24,7 @@ GEN_DOCS=false
 DOCS_FORMAT="html,index.html"
 LINT=false
 LINT_CHECKS=""
-SUPPORTED_LANGUAGES=("go" "ruby" "csharp" "java" "python" "objc" "gogo" "php" "node" "web" "cpp")
+SUPPORTED_LANGUAGES=("go" "ruby" "csharp" "java" "python" "objc" "gogo" "php" "node" "web" "cpp" "descriptor_set")
 EXTRA_INCLUDES=""
 OUT_DIR=""
 GO_SOURCE_RELATIVE=""
@@ -180,6 +180,9 @@ plugins=grpc+embedded\
         ;;
     "web")
         GEN_STRING="--grpc-web_out=import_style=typescript,mode=grpcwebtext:$OUT_DIR --js_out=import_style=commonjs:$OUT_DIR --plugin=protoc-gen-grpc-web=`which grpc_${PLUGIN_LANG}_plugin`"
+        ;;
+    "descriptor_set")
+        GEN_STRING="--descriptor_set_out=$OUT_DIR/descriptor_set.pb"
         ;;
     *)
         GEN_STRING="--grpc_out=$OUT_DIR --${GEN_LANG}_out=$OUT_DIR --plugin=protoc-gen-grpc=`which grpc_${PLUGIN_LANG}_plugin`"
