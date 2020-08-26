5b67bff84090d6d32518d87cf5f6193f283ffe05
Abraham Heidebrecht <abraham.heidebrecht@namely.com>
2020-08-26 12:41:09 -0400

    Try to build grpc ourselves still

--- all/install-protobuf.sh
+++ all/install-protobuf.sh
@@ -18,10 +18,26 @@ make install
 
 cd /tmp
 
-git clone -b v$1.x --recursive -j8 https://github.com/grpc/grpc
-cd /tmp/grpc
-CFLAGS="-Wno-cast-function-type" make
-make install
+git clone -b v$1.x --recursive -j8 --depth 1 https://github.com/grpc/grpc
+mkdir -p /tmp/grpc/cmake/build
+cd /tmp/grpc/cmake/build
+# gRPC_BUILD_TESTS for grpc_cli - very slow
+cmake ../..                      \
+    -DgRPC_INSTALL=ON            \
+    -DgRPC_ZLIB_PROVIDER=package \
+    -DgRPC_SSL_PROVIDER=package  \
+    -DCMAKE_INSTALL_PREFIX=/opt  \
+	-DBUILD_TESTING=OFF          \
+	-DgRPC_BUILD_TESTS=ON
+RUN make
+RUN make install
+
+# Workaround for the transition to protoc-gen-go-grpc
+# https://grpc.io/docs/languages/go/quickstart/#regenerate-grpc-code
+cd /tmp
+git clone -b v$grpc.x --recursive https://github.com/grpc/grpc-go.git
+( cd ./grpc-go/cmd/protoc-gen-go-grpc && go install . )
+
 # php support
 git submodule update --init
 make grpc_php_plugin
