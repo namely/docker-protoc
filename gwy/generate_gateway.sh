1d28cc3b844c439196bc8036bbfd67d20c86f778
Kevin Samoei <samoeikev@gmail.com>
2021-01-21 17:02:54 +0300

    pin gateway

--- gwy/generate_gateway.sh
+++ gwy/generate_gateway.sh
@@ -132,6 +132,7 @@ SWAGGER_FILE_NAME=`basename $PROTO_FILE .proto`.swagger.json
 
 # Copy and update the templates.
 renderizer --import=${GATEWAY_IMPORT_DIR} --swagger=${SWAGGER_FILE_NAME} /templates/config.yaml.tmpl > $OUT_DIR/config.yaml
+renderizer --import=${GATEWAY_IMPORT_DIR} --swagger=${SWAGGER_FILE_NAME} /templates/go.mod.tmpl > $OUT_DIR/go.mod
 renderizer --import=${GATEWAY_IMPORT_DIR} --swagger=${SWAGGER_FILE_NAME} /templates/Dockerfile.tmpl > $OUT_DIR/Dockerfile
 
 MAIN_DIR=${OUT_DIR}/cmd/gateway
