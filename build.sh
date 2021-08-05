4126d2210802923389c0dcaade5adca1c1b3a82b
Ido David <36866853+ido-namely@users.noreply.github.com>
2021-08-05 17:41:35 -0400

    switch to Github Actions CI (#249)
    * switch to github actions for ci in PRs
    
    * add test to pr ci, remove azure pipeline
    
    * fix env var
    
    * fix env var
    
    * fix env in ci
    
    * fix env
    
    * fix env
    
    * fix env
    
    * add test step back
    
    * fix test permissions
    
    * new line
    
    * update readme
    
    * fix missing var
    
    * adding release configuration
    
    * update readme, adjust manual execution of build
    
    * fix comment, new line
    
    * update readme with better release instructions

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 source ./variables.sh
 
 for build in ${BUILDS[@]}; do
-    tag=${CONTAINER}/${build}:${GRPC_VERSION}_${BUILD_VERSION}
+    tag=${CONTAINER}/${build}:${BUILD_VERSION}
     echo "building ${build} container with tag ${tag}"
     docker build -t ${tag} \
         -f Dockerfile \
--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@
 source ./variables.sh
 
 for build in ${BUILDS[@]}; do
-    tag=${CONTAINER}/${build}:${GRPC_VERSION}_${BUILD_VERSION}
+    tag=${CONTAINER}/${build}:${BUILD_VERSION}
     echo "pushing ${tag}"
     docker push ${tag}
 
